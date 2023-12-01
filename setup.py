d3a70c0066714cb84f1da27603cd8ff2a8efc2c8
gbo999 <gbo10001@gmail.com>
2023-12-01 17:42:35 +0200

    inital commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='developing counting algorithms for aquaculture monitoring robot',
+    author='Gil Ben Or',
+    license='MIT',
+)
