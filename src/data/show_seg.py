85710f5657481f0aeafda8279372ae1764e54a03
gbo999 <gbo10001@gmail.com>
2023-12-26 23:32:17 +0200

    diameter with length

--- src/data/show_seg.py
+++ src/data/show_seg.py
@@ -13,7 +13,7 @@ def draw_polygon(img, class_id, points):
 
     return pts
 
-def draw_diameter_line(img, points):
+def draw_diameter_line(img, points, focal_length, distance_to_object, pixel_size, original_size, resized_size):
     # Find the two farthest points in the segmentation
     max_distance = 0
     point1 = points[0][0]
@@ -24,7 +24,6 @@ def draw_diameter_line(img, points):
             p1 = points[i][0]
             p2 = points[j][0]
             distance = np.linalg.norm(p1 - p2)
-            
             if distance > max_distance:
                 max_distance = distance
                 point1 = p1
@@ -32,8 +31,50 @@ def draw_diameter_line(img, points):
 
     # Draw the diameter line
     cv2.line(img, tuple(point1), tuple(point2), (0, 0, 255), 2)
+    
+    # Calculate the scaling factor due to image resizing
+    scale_width = original_size[0] / resized_size[0]
+    scale_height = original_size[1] / resized_size[1]
+    
+    # Assuming the scaling factor is the same for width and height
+    # If not, you need to calculate the width and height separately
+    scale_factor = (scale_width + scale_height) / 2
+    
+    # Scale the pixel measurement back to original dimensions
+    width_in_original_pixels = int(max_distance * scale_factor)
 
+    # Calculate the real-life width of the object
+    real_width_cm = calculate_real_width(focal_length, distance_to_object, width_in_original_pixels, pixel_size)
+    
+    # Draw the real-life width as text on the image
+    text_position = (min(point1[0], point2[0]), min(point1[1], point2[1]) - 10)  # Adjust text position as needed
+    cv2.putText(img, f"{real_width_cm:.2f} mm", text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
+ # Resized image size (width, height)
+
+def calculate_real_width(focal_length, distance_to_object, width_in_pixels, pixel_size):
+    """
+    Calculate the real-life width of an object.
+
+    Parameters:
+    focal_length (float): Focal length of the camera lens in millimeters (mm).
+    distance_to_object (float): Distance from the camera to the object in millimeters (mm).
+    width_in_pixels (int): Width of the object in pixels on the image sensor.
+    pixel_size (float): Size of a pixel on the image sensor in millimeters (mm).
+
+    Returns:
+    float: Real-life width of the object in centimeters (cm).
+    """
+    # Calculate the width of the object in the image sensor plane in millimeters
+    width_in_sensor = width_in_pixels * pixel_size
+
+    # Calculate the real-life width of the object using the similar triangles principle
+    real_width_mm = (width_in_sensor * distance_to_object) / focal_length
+
+    # Convert the width from millimeters to centimeters
+    real_width_cm = real_width_mm 
+
+    return real_width_cm
 
 def process_image(image_path, label_path):
     # Load the image and resize
@@ -49,7 +90,7 @@ def process_image(image_path, label_path):
             class_id = int(parts[0])
             points = [(float(parts[i]), float(parts[i + 1])) for i in range(1, len(parts) - 1, 2)]
             pts = draw_polygon(img, class_id, points)
-            draw_diameter_line(img, pts)
+            draw_diameter_line(img, pts,focal_length=24.4, distance_to_object=700, pixel_size=0.00716844, original_size=(5312, 2988), resized_size=(640, 360))
 
     # Display the image
     cv2.imshow('Segmented Image with Diameters', img)
