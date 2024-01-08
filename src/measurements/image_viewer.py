8f21be3f6f88db90e1441da8df1861a74378358b
gbo999 <gbo10001@gmail.com>
2024-01-08 17:02:56 +0200

    a lot

--- src/measurements/image_viewer.py
+++ src/measurements/image_viewer.py
@@ -14,6 +14,7 @@ class ImageViewer:
 
     def _load_images(self):
         images = [img for img in sorted(os.listdir(self.image_dir)) if img.endswith(('.png', '.jpg', '.jpeg'))]
+        print( f'len {len(images)}')
         return images
 
     def _load_labels(self):
@@ -33,42 +34,34 @@ class ImageViewer:
     def _draw_annotations(self, image):
         label_path = os.path.join(self.label_dir, self.labels[self.current_image_index])
         with open(label_path, 'r') as file:
-            segmentations = file.readlines()
-            if self.current_segmentation_index < len(segmentations):
-                line = segmentations[self.current_segmentation_index]
-                self._draw_single_annotation(image, line)
+           for line in file:
+            parts = line.split()
+            points = [(float(parts[i]), float(parts[i + 1])) for i in range(1, len(parts) - 1, 2)]
+            self._draw_single_annotation(image, points)
 
-
-    def _draw_single_annotation(self, image, line):
-    # Extracting the segmentation points
-        parts = line.strip().split()
-        class_id = parts[0]
-        normalized_points = [float(p) for p in parts[1:]]
-        
-        # Convert normalized values to pixel values
-        pixel_points = self._convert_to_pixels(normalized_points, image.shape[1], image.shape[0])
-        
-        # Draw the segmentation mask (contour) and enclosing circle
-        if len(pixel_points) > 2:
+    def _draw_single_annotation(self, image, points):
+        if len(points) > 2:
+            pixel_points = self._convert_to_pixels(points, image.shape[1], image.shape[0])
             cv2.polylines(image, [np.array(pixel_points, dtype=np.int32)], isClosed=True, color=(0, 255, 0), thickness=2)
-            center, radius = minimum_enclosing_circle(pixel_points)
-            if center and radius:
-                cv2.circle(image, (int(center[0]), int(center[1])), int(radius), (0, 0, 255), 2)
-    
+            # center, radius = minimum_enclosing_circle(pixel_points)
+            # if center and radius:
+            #     cv2.circle(image, (int(center[0]), int(center[1])), int(radius), (0, 0, 255), 2)
+
     def _convert_to_pixels(self, normalized_points, image_width, image_height):
         pixel_points = []
-        for i in range(0, len(normalized_points), 2):
-            x_pixel = int(normalized_points[i] * image_width)
-            y_pixel = int(normalized_points[i + 1] * image_height)
+        print(f'len {len(normalized_points)}')
+        for x_normalized, y_normalized in normalized_points:
+            x_pixel = int(x_normalized * image_width)
+            y_pixel = int(y_normalized * image_height)
             pixel_points.append((x_pixel, y_pixel))
         return pixel_points
 
     def navigate_images(self, key):
-        if key == 'd' and self.current_index < len(self.images) - 1:
-            self.current_index += 1
+        if key == 'd' and self.current_image_index < len(self.images) - 1:
+            self.current_image_index += 1
             self.current_segmentation_index = 0  # Reset segmentation index
-        elif key == 'a' and self.current_index > 0:
-            self.current_index -= 1
+        elif key == 'a' and self.current_image_index > 0:
+            self.current_image_index -= 1
             self.current_segmentation_index = 0  # Reset segmentation index
 
     def navigate_segmentations(self, key):
